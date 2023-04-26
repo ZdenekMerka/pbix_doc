e29157ddee0b4f6888f85d660885c342af85211d
Zdenek Merka <zdenek.merka@solitea.cz>
2023-04-26 23:35:44 +0200

    add licence

--- pbix_doc/bin/pbix_doc.py
+++ pbix_doc/bin/pbix_doc.py
@@ -1,3 +1,20 @@
+# 
+# Copyright (C) 2023 Zdenek Merka
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 ##############################
 # how to run 
 # python .\bin\pbix_doc.py --conn_id aw_sales
@@ -80,6 +97,12 @@ for instance_folder in ssas_instancie_folders:
     # init writer 
     writer =  w.pbix_writer(md,None, common_conf['output'])
     writer.init_templates()
+    #writer.render_property()
+    #writer.render_tables()
+    #writer.render_columns()
+    #writer.render_measures()
+    #writer.render_hierarchies()
+    writer.render_relationships()
     #writer.write_metadata()
 
 
